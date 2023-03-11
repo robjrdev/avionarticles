9658eb91c6ef60ac7b6a8493cb497122b4a19fb8
robjrdev <robertjr@DESKTOP-VCKJ24P.localdomain>
2023-03-11 13:29:27 +0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get '/articles', to: 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'new_article'
+  post '/articles' => 'articles#create', as: 'create_article'
+  get '/articles/:id' => 'articles#show', as: 'article'
+  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+  # put '/articles/:id' => 'articles#update'
+  patch '/articles/:id' => 'articles#update'
+  delete '/articles/:id' => 'articles#destroy'
+  # Defines the root path route ("/")
+  root 'articles#index'
+end
